a7fc4336aa061a110f8f7163411909efaa87a55a
Matthew R. Wilson <mwilson@mattwilson.org>
2019-03-23 22:10:56 -0700

    Callsign tracking

--- consolehandler/handler.go
+++ consolehandler/handler.go
@@ -8,36 +8,63 @@ import (
 )
 
 type ConsoleHandler struct {
+	callsigns map[string]string
 }
 
-func (h ConsoleHandler) NewFlight(icaoID string, firstSeen time.Time) {
-	fmt.Println("New flight :", icaoID)
+func (h *ConsoleHandler) NewFlight(icaoID string, firstSeen time.Time) {
+	fmt.Printf("%8s: New flight created.\n", icaoID)
 }
 
-func (h ConsoleHandler) CloseFlight(icaoID string, lastSeen time.Time, messages int) {
-	fmt.Printf("Closed flight %s after seeing %d messages\n", icaoID, messages)
+func (h *ConsoleHandler) CloseFlight(icaoID string, lastSeen time.Time, messages int) {
+	fmt.Printf("%8s: Closed after %d messages\n", h.bestID(icaoID), messages)
+	delete(h.callsigns, icaoID)
 }
 
-func (h ConsoleHandler) SetIdentity(icaoID, callsign string, change bool) {
-	fmt.Printf("New callsign for %s: %s\n", icaoID, callsign)
+func (h *ConsoleHandler) SetIdentity(icaoID, callsign string, change bool) {
+	if h.callsigns == nil {
+		h.callsigns = make(map[string]string)
+	}
+
+	h.callsigns[icaoID] = callsign
+	fmt.Printf("%8s: New callsign for %s", callsign, icaoID)
 	if change {
-		fmt.Println("*** CHANGE ***")
-	}
-}
-
-func (h ConsoleHandler) AddTrackPoint(icaoID string, trackPoint tracker.TrackLog) {
-	fmt.Printf("New track log for %s.", icaoID)
-	if trackPoint.SpeedValid {
-		fmt.Printf(" Speed %d.", trackPoint.Speed)
-	}
-	if trackPoint.HeadingValid {
-		fmt.Printf(" Heading: %d.", trackPoint.Heading)
-	}
-	if trackPoint.VSValid {
-		fmt.Printf(" VS: %d.", trackPoint.VS)
-	}
-	if trackPoint.AltitudeValid {
-		fmt.Printf(" Alt: %d.", trackPoint.Altitude)
+		fmt.Printf("*** CHANGE ***")
 	}
 	fmt.Printf("\n")
+}
+
+func (h *ConsoleHandler) AddTrackPoint(icaoID string, trackPoint tracker.TrackLog) {
+	fmt.Printf("%8s:", h.bestID(icaoID))
+	if trackPoint.HeadingValid {
+		fmt.Printf(" %03d°", trackPoint.Heading)
+	} else {
+		fmt.Printf("     ")
+	}
+	if trackPoint.SpeedValid {
+		fmt.Printf(" %3dkts", trackPoint.Speed)
+	} else {
+		fmt.Printf("       ")
+	}
+	if trackPoint.AltitudeValid {
+		fmt.Printf(" %5dft", trackPoint.Altitude)
+	} else {
+		fmt.Printf("        ")
+	}
+	if trackPoint.VSValid {
+		fmt.Printf(" %5dfpm", trackPoint.VS)
+	} else {
+		fmt.Printf("         ")
+	}
+	fmt.Printf("\n")
+}
+
+func (h *ConsoleHandler) bestID(icao string) string {
+	if h.callsigns == nil {
+		h.callsigns = make(map[string]string)
+	}
+
+	if callsign, ok := h.callsigns[icao]; ok {
+		return callsign
+	}
+	return icao
 }
