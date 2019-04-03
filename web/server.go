3507d062a1a078d96ac2b20529be9287390d13f2
Matthew R. Wilson <mwilson@mattwilson.org>
2019-04-02 20:29:03 -0700

    Flight list on web server

--- web/server.go
+++ web/server.go
@@ -1,29 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 	"text/template"
+	"time"
 
+	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	db, err := getConnection()
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	e := echo.New()
 	t := &Template{
 		templates: template.Must(template.ParseGlob("templates/*.html")),
 	}
 	e.Renderer = t
 	e.Use(middleware.Logger())
-	e.GET("/", func(c echo.Context) error {
-		vals := struct {
-			Title string
-		}{
-			Title: "page title 2",
-		}
-		return c.Render(http.StatusOK, "page.html", vals)
-	})
+	e.GET("/", getFlightsHandler(db))
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -33,4 +39,59 @@ type Template struct {
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+type FlightsRow struct {
+	ID           int            `db:"id"`
+	Icao         string         `db:"icao"`
+	Callsign     sql.NullString `db:"callsign"`
+	FirstSeen    time.Time      `db:"first_seen"`
+	LastSeen     time.Time      `db:"last_seen"`
+	MsgCount     int            `db:"msg_count"`
+	Registration sql.NullString
+	Owner        sql.NullString
+}
+
+func getFlightsHandler(db *sqlx.DB) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		flights := []FlightsRow{}
+		start := time.Date(2019, time.April, 1, 0, 0, 0, 0, time.Local).UTC()
+		end := time.Date(2019, time.April, 2, 0, 0, 0, 0, time.Local).UTC()
+		err := db.Select(&flights,
+			`SELECT f.id, f.icao, f.callsign, f.first_seen, f.last_seen, f.msg_count,
+			        r.registration, r.owner
+			 FROM flight f
+			 LEFT OUTER JOIN registration r ON f.icao=r.icao
+			 WHERE f.first_seen >= $1 AND f.first_seen < $2
+			 ORDER BY f.first_seen`,
+			start, end)
+		if err != nil {
+			c.Logger().Error(err)
+			return err
+		}
+
+		for i := range flights {
+			if flights[i].Owner.Valid {
+				flights[i].Owner.String = strings.Title(strings.ToLower(flights[i].Owner.String))
+			}
+		}
+
+		vals := struct {
+			Title   string
+			Flights []FlightsRow
+		}{
+			Title:   "Flights",
+			Flights: flights,
+		}
+		return c.Render(http.StatusOK, "flightlist.html", vals)
+	}
+}
+
+func getConnection() (*sqlx.DB, error) {
+	connStr, ok := os.LookupEnv("DBURL")
+	if !ok {
+		return nil, fmt.Errorf("DBURL environment variable not set")
+	}
+	db, err := sqlx.Connect("postgres", connStr)
+	return db, err
 }
